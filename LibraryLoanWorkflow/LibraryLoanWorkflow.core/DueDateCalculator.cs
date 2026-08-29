0c37e517ead12f1479adbb0543767d7a9ae212fa
Kalyani <168560682+Kalyani1318456789@users.noreply.github.com>
2026-08-29 23:19:32 +0530

    Update DueDateCalculator.cs

--- LibraryLoanWorkflow/LibraryLoanWorkflow.core/DueDateCalculator.cs
+++ LibraryLoanWorkflow/LibraryLoanWorkflow.core/DueDateCalculator.cs
@@ -1,6 +1,7 @@
 ﻿namespace LibraryLoanWorkflow.Core;
 public sealed class DueDateCalculator
 {
+    // A library loan is due 30 days after the borrowed date.
     public DateOnly Calculate(Loan loan)
         => loan.BorrowedOn.AddDays(30);
 }
